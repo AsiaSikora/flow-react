2a6f13d9b114c1c94d3ddc1b369bb16402b65b83
Therion95 <piotrpawelczyk95@gmail.com>
2022-02-27 17:32:17 +0100

    Added NewMeasurement in App function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import Menu from './components/Menu/Menu';
 import FetchApi from './FetchApi';
 import FetchApiHook from './FetchApiHook';
 import HomePage from './components/HomePage/HomePage';
+import NewMeasurement from './components/NewMeasurement/NewMeasurement';
 import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy'
 
 function App() {
-  return (
-    <div >
-      <Menu />
-      <HomePage />
-      <PrivacyPolicy/>
-    </div>
-  );
+    return (
+        <div>
+            <Menu/>
+            {/*<HomePage />*/}
+            <NewMeasurement/>
+            <PrivacyPolicy/>
+        </div>
+    );
 }
 
 export default App;

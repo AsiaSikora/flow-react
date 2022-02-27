2a2479cabc8bd59adb0f74d6ef39d0772713f67d
Therion95 <piotrpawelczyk95@gmail.com>
2022-02-27 22:40:22 +0100

    Added title for form.select and new button.

--- src/components/NewMeasurement/SelectLocalization/SelectLocalization.js
+++ src/components/NewMeasurement/SelectLocalization/SelectLocalization.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {Form} from "react-bootstrap";
+import ButtonDefault from "../../ButtonDefault/ButtonDefault";
 
 function SelectLocalization(props) {
     return (
         <div>
+            <h3>Select localization</h3>
             <Form.Select>
-                {props.localizations
-                .map(loc => <option key={loc.id} {...loc}>{loc.name}</option>)}
+                {props.localizations.map(loc => <option key={loc.id} {...loc}>{loc.name}</option>)}
             </Form.Select>
+            <ButtonDefault title="Add new localization"/>
         </div>
     )
 }

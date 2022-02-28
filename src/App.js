f40967dc0e585a236532939528f366986c1606bf
KamilZ13 <kamzor1331@gmail.com>
2022-02-28 21:19:18 +0100

    Implemented SpecialPoints component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import HomePage from './components/HomePage/HomePage';
 import NewMeasurement from './components/NewMeasurement/NewMeasurement';
 import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
 import Details from './components/Details/Details';
+import SpecialPoints from './components/Details/SpecialPoints/SpecialPoints';
+
 
 function App() {
     return (
         <div>
             <Menu/>
+            <SpecialPoints/>
             <Details/>
             <NewMeasurement/>
             <HomePage />
---
+++ src/components/Details/SpecialPoints/SpecialPoints.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+function SpecialPoints() {
+    return (
+        <div className="container">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                    <th scope="col">Time</th>
+                    <th scope="col">Flow</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default SpecialPoints;

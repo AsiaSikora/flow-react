c5b6905f59a10ea45090ede7e8ed674d0191ab8a
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2022-03-05 21:42:59 +0100

    SurveyDetails styling

--- src/components/Details/SurveyDetails/SurveyDetails.js
+++ src/components/Details/SurveyDetails/SurveyDetails.js
@@ -8,12 +8,12 @@ function SurveyDetails(props) {
         <div className={`${styles.position} container`}>
             <div class="card" >
                 <div className="card-header">
-                Survey date: {(props.survey.date).slice(0,10)}
+                <b>Survey date: {(props.survey.date).slice(0,10)}</b>
                 </div>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">Device number: {props.survey.device.deviceNumber}</li>
                     <li className="list-group-item">Localization name: {props.survey.localization.name}</li>
-                    <li className="list-group-item">Average flow: {(averageFlows[len-1]).toString().slice(0,5)}</li>
+                    <li className="list-group-item">Average flow: <b>{(averageFlows[len-1]).toString().slice(0,5)}</b></li>
                 </ul>
             </div>
         </div>

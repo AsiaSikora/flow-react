df9a73bb611ebba494eda025cceb3cb5d636aa01
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2022-03-15 19:38:40 +0100

    CurrentMeasurementDetails refactor

--- src/components/NewMeasurement/CurrentMeasurement/CurrentMeasurementDetails/CurrentMeasurementDetails.js
+++ src/components/NewMeasurement/CurrentMeasurement/CurrentMeasurementDetails/CurrentMeasurementDetails.js
@@ -3,23 +3,23 @@ import styles from './CurrentMeasurementDetails.module.css'
 function CurrentMeasurementDetails(props) {
 
     let averageFlows = props.survey.measurements.map(el => el.averageFlow);
-    let len = averageFlows.length;
     let flows = props.survey.measurements.map(el => el.currentFlow);
     let temperatures = props.survey.measurements.map(el => el.temperature);
     let pressures = props.survey.measurements.map(el => el.pressure);
+    let last = averageFlows.length - 1;
 
     return(
         <div>
-            {len > 0 ? 
+            {last >= 0 ? 
             <div className="d-flex justify-content-center">
-            <div className={` ${styles.position} card `} >
-                <ul className="list-group list-group-flush">
-                <li className="list-group-item">Current flow: {flows[len-1]} m&#179;/s</li>
-                <li className="list-group-item">Average flow: <b>{(averageFlows[len-1])} m&#179;/s</b></li>
-                <li className="list-group-item">Pressure: {pressures[len-1]} hPa</li>
-                <li className="list-group-item">Temperature: {temperatures[len-1]} &#176;C</li>
-                </ul>
-            </div>
+                <div className={` ${styles.position} card `} >
+                    <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Current flow: {flows[last]} m&#179;/s</li>
+                    <li className="list-group-item">Average flow: <b>{(averageFlows[last]).toString().slice(0,5)} m&#179;/s</b></li>
+                    <li className="list-group-item">Pressure: {pressures[last]} hPa</li>
+                    <li className="list-group-item">Temperature: {temperatures[last]} &#176;C</li>
+                    </ul>
+                </div>
             </div> : ""}
         </div>
         
